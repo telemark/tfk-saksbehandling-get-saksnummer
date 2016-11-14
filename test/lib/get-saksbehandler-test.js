26a11fb0c4d5a50ac0afcc1f95ae60b917ceb618
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-14 09:54:14 +0100

    Adds saksnummer

--- test/lib/get-saksbehandler-test.js
+++ test/lib/get-saksbehandler-test.js
@@ -3,6 +3,6 @@
 const tap = require('tap')
 const getSaksnummer = require('../../index')
 
-tap.isEqual(Object.keys(getSaksnummer()).length, 9, 'returns all on empty')
+tap.isEqual(Object.keys(getSaksnummer()).length, 8, 'returns all on empty')
 
-tap.ok(getSaksnummer('skoleskyss', 'returns expected for skoleskyss'))
+tap.ok(getSaksnummer('tilskudd-folkehelse', 'returns expected for tilskudd-folkehelse'))
