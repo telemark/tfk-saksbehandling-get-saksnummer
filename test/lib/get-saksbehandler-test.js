55ce6fadcd01d03502c30eccaddbeb2fec95fe83
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-22 09:27:46 +0100

    Adds test for undefined

--- test/lib/get-saksbehandler-test.js
+++ test/lib/get-saksbehandler-test.js
@@ -6,3 +6,5 @@ const getSaksnummer = require('../../index')
 tap.isEqual(Object.keys(getSaksnummer()).length, 8, 'returns all on empty')
 
 tap.ok(getSaksnummer('tilskudd-folkehelse', 'returns expected for tilskudd-folkehelse'))
+
+tap.isEqual(undefined, getSaksnummer('this-does-not-exist'), 'returns undefined if not found')
