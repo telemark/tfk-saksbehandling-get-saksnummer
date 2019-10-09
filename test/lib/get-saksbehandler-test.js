cf0c8143b19185dcaf9dfaae9ece39ccb36cf45b
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-10-09 09:27:28 +0200

    Updates data and tests (patch)

--- test/lib/get-saksbehandler-test.js
+++ test/lib/get-saksbehandler-test.js
@@ -3,22 +3,12 @@ const getSaksnummer = require('../../index')
 
 tap.ok(getSaksnummer, 'module loads OK')
 
-tap.isEqual(Object.keys(getSaksnummer()).length, 8, 'returns all on empty')
+tap.isEqual(Object.keys(getSaksnummer()).length, 3, 'returns all on empty')
 
 tap.ok(getSaksnummer('tilskudd-folkehelse'), 'returns expected for tilskudd-folkehelse')
 
 tap.ok(getSaksnummer('tilskudd-idrett'), 'returns expected for tilskudd-idrett')
 
-tap.ok(getSaksnummer('tilskudd-kultur-billedkunst'), 'returns expected for tilskudd-kultur-billedkunst')
-
-tap.ok(getSaksnummer('tilskudd-kultur-film'), 'returns expected for tilskudd-kultur-film')
-
-tap.ok(getSaksnummer('tilskudd-kultur-kulturarv'), 'returns expected for tilskudd-kultur-kulturarv')
-
-tap.ok(getSaksnummer('tilskudd-kultur-litteratur'), 'returns expected for tilskudd-kultur-litteratur')
-
-tap.ok(getSaksnummer('tilskudd-kultur-musikk'), 'returns expected for tilskudd-kultur-musikk')
-
-tap.ok(getSaksnummer('tilskudd-kultur-scenekunst'), 'returns expected for tilskudd-kultur-scenekunst')
+tap.ok(getSaksnummer('tilskudd-kultur'), 'returns expected for tilskudd-kultur')
 
 tap.isEqual(undefined, getSaksnummer('this-does-not-exist'), 'returns undefined if not found')
